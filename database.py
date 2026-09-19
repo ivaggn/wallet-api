5c81a7276e664d9576366211d45cd038a723b926
Ivan Deshko <X00228540@mytudublin.ie>
2026-09-19 18:56:14 +0100

    Add databse

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+engine = create_engine("sqlite:///./wallet.db", 
+                       connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
+class Base(DeclarativeBase): pass
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
